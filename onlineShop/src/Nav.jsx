f1b8fe422b09c69d41f29a1aea9375ba9adb3994
radhe <radheshyamlodhi48@gmail.com>
2024-08-01 00:08:53 +0530

    remove nav element if user loggedIn then dont showup  signup button

--- onlineShop/src/Nav.jsx
+++ onlineShop/src/Nav.jsx
@@ -1,17 +1,19 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 function Nav() {
+  const auth = localStorage.getItem("user");
   return (
     <nav className="w-full h-12 bg-slate-800 text-white flex gap-10 font-semibold text-xl justify-center items-center ">
-
-       <Link to="/">Products</Link>
-       <Link to="/add">Add Products</Link>
-       <Link to="/update">Update Products</Link>
-       <Link to="/logout">logout</Link>
-       <Link to="/profile">Profile</Link>
-       <Link to="/signup">SignUp</Link>
-          
-    </nav >
-  )
+      <Link to="/">Products</Link>
+      <Link to="/add">Add Products</Link>
+      <Link to="/update">Update Products</Link>
+      <Link to="/profile">Profile</Link>
+      {auth ? (
+        <Link to="/logout">logout</Link>
+      ) : (
+        <Link to="/signup">SignUp</Link>
+      )}
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
